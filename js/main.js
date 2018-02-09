a8d966dce3b552c3d6075008c9945eeeb9acf653
tmac9494 <trentmcdole94@gmail.com>
2018-02-09 16:53:55 -0600

    new functionality
    can now add multiple sliders by calling tRunSlider('.element') on each slider in the main.js file also can add the class text-slider to the slider to create a responsive text slider instead of image. also the slider will max out height at 650px and turn the image into a backgroud image that scales with size

--- js/main.js
+++ js/main.js
@@ -1,111 +1,146 @@
 $(document).ready(function() {
 //------------------trent's slider
-		$offset = $('.trent-slider').width();
-		$tSlideInStyles = {left: '0', right: '0'}
-		$t_loadBarStopStyles = {animation : "none", width : "0%"}
-		$hiddenSlideStylesRight = {left: $offset, right: 0 - $offset}
-		$hiddenSlideStylesLeft = {right: $offset, left: 0 - $offset}
-		
-		//slider functions
-		function tStartLoadBar() {$('.t-load-bar .inner-load-bar').css('animation', 'load 4.5s linear infinite');}
+function tRunSlider(sliderName) {
+	$(sliderName).addClass('trent-slider');
+	$offset = $(sliderName).width();
+	$tSlideInStyles = {left: '0', right: '0'}
+	$t_loadBarStopStyles = {animation : "none", width : "0%"}
+	$hiddenSlideStylesRight = {left: $offset, right: 0 - $offset}
+	$hiddenSlideStylesLeft = {right: $offset, left: 0 - $offset}
 
-		function tSliderHasStopped() {
-			if ($('.current-t-slide').css('left') === "0px" && $('.current-t-slide').css('right') === "0px") {
-				return true;
+	//slider functions
+	function setTextSlideHeight()  {
+		if ($(sliderName).hasClass('text-slider')) {
+			if ($(window).width() >= 768 ) {
+				$(''+ sliderName +'.text-slider').css('height', $(''+ sliderName +' .current-t-slide .t-slide-content').height() + 245);
+				if ($(''+ sliderName +'.text-slider').hasClass('small-slider')) {
+					$(''+ sliderName +'.text-slider.small-slider').css('height', $(''+ sliderName +' .current-t-slide .t-slide-content').height() + 100);
+				}
 			} else {
-				return false;
+				$(''+ sliderName +'.text-slider').css('height', $(''+ sliderName +' .current-t-slide .t-slide-content').height() + 70);
 			}
 		}
+	}
 
-		function tSlideChangerRight() {
-			if ($('.current-t-slide').next().hasClass('t-slide') && tSliderHasStopped()) {
-				$('.current-t-slide').removeClass('current-t-slide').css($hiddenSlideStylesLeft).next().css($tSlideInStyles).addClass('current-t-slide');
-				$('.current-dot').removeClass('current-dot').next().addClass('current-dot');
-			} else if (tSliderHasStopped()) {
-				$('.current-t-slide').removeClass('current-t-slide');
-				$('.t-slide').first().addClass('current-t-slide').css($tSlideInStyles);
-				tSetCss();
-				$('.current-dot').removeClass('current-dot')
-				$('.t-dot').first().addClass('current-dot');
+	function tStartLoadBar() {$(''+ sliderName +' .t-load-bar .inner-load-bar').css('animation', 'load 4.5s linear infinite');}
+
+	function tSliderHasStopped() {
+		if ($('.current-t-slide').css('left') === "0px" && $(''+ sliderName +' .current-t-slide').css('right') === "0px") {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function tSlideChangerRight() {
+		if ($(''+ sliderName +' .current-t-slide').next().hasClass('t-slide') && tSliderHasStopped()) {
+			$(''+ sliderName +' .current-t-slide').removeClass('current-t-slide').css($hiddenSlideStylesLeft).next().css($tSlideInStyles).addClass('current-t-slide');
+			$(''+ sliderName +' .current-dot').removeClass('current-dot').next().addClass('current-dot');
+			setTextSlideHeight();
+		} else if (tSliderHasStopped()) {
+			$(''+ sliderName +' .current-t-slide').removeClass('current-t-slide');
+			$(''+ sliderName +' .t-slide').first().addClass('current-t-slide').css($tSlideInStyles);
+			tSetCss();
+			$(''+ sliderName +' .current-dot').removeClass('current-dot')
+			$(''+ sliderName +' .t-dot').first().addClass('current-dot');
+			setTextSlideHeight();
+		}
+	}
+	function tSlideChangerLeft() {
+		if ($(''+ sliderName +' .current-t-slide').prev().hasClass('t-slide') && tSliderHasStopped()) {
+			$(''+ sliderName +' .current-t-slide').removeClass('current-t-slide').css($hiddenSlideStylesRight).prev().css($tSlideInStyles).addClass('current-t-slide');
+			$(''+ sliderName +' .current-dot').removeClass('current-dot').prev().addClass('current-dot');
+			setTextSlideHeight();
+		} else if (tSliderHasStopped()) {
+			$(''+ sliderName +' .current-t-slide').removeClass('current-t-slide');
+			$(''+ sliderName +' .t-slide').last().addClass('current-t-slide').css($tSlideInStyles);
+			tSetCssLeft();
+			$(''+ sliderName +' .current-dot').removeClass('current-dot')
+			$(''+ sliderName +' .t-dot').last().addClass('current-dot');
+			setTextSlideHeight();
+		}
+	}
+
+	function tSetCss() {
+		$(''+ sliderName +' .t-slide').each(function(index, value) {
+			if (index > 0) {
+				$(this).css($hiddenSlideStylesRight);
 			}
-		}
-		function tSlideChangerLeft() {
-			if ($('.current-t-slide').prev().hasClass('t-slide') && tSliderHasStopped()) {
-				$('.current-t-slide').removeClass('current-t-slide').css($hiddenSlideStylesRight).prev().css($tSlideInStyles).addClass('current-t-slide');
-				$('.current-dot').removeClass('current-dot').prev().addClass('current-dot');
-			} else if (tSliderHasStopped()) {
-				$('.current-t-slide').removeClass('current-t-slide');
-				$('.t-slide').last().addClass('current-t-slide').css($tSlideInStyles);
-				tSetCssLeft();
-				$('.current-dot').removeClass('current-dot')
-				$('.t-dot').last().addClass('current-dot');
+		});
+	}
+	function tSetCssLeft() {
+		$t_total = $(''+ sliderName +' .t-slide').length - 1;
+		$(''+ sliderName +' .t-slide').each(function(index, value) {
+			if (index < $t_total) {
+				$(this).css($hiddenSlideStylesLeft)
 			}
-		}
-
-		function tSetCss() {
-			$('.t-slide').each(function(index, value) {
-				if (index > 0) {
-					$(this).css($hiddenSlideStylesRight);
-				}
-			});
-		}
-		function tSetCssLeft() {
-			$t_total = $('.t-slide').length - 1;
-			$('.t-slide').each(function(index, value) {
-				if (index < $t_total) {
-					$(this).css($hiddenSlideStylesLeft)
-				}
-			});
-		}
+		});
+	}
 
 
-		//populate dots for every slide
+	//populate dots for every slide
+	$(''+ sliderName +' .t-slide').each(function(index, value) {
+		$(''+ sliderName +' .t-slide-dots').append('<div class="t-dot"></div>');
+		if (index === 0) {$(''+ sliderName +' .t-dot').first().addClass('current-dot')}
+	});
+
+	//slider-code
+	$tSliderHeight = $('.trent-slider').width() / 2;
+	if ($tSliderHeight > 650) {
 		$('.t-slide').each(function(index, value) {
-			$('.t-slide-dots').append('<div class="t-dot"></div>');
-			if (index === 0) {$('.t-dot').first().addClass('current-dot')}
+				$src = $('.t-slide').eq(index).find('img').attr('src');
+				$slideBgStyles = {backgroundImage: 'url(' + $src + ')', backgroundSize: 'cover',backgroudRepeat: 'no-repeat',backgroundPosition: 'center'}
+				$(this).css($slideBgStyles);
+				$(this).find('img').first().hide();
 		});
-
-		//slider-code
-		$('.trent-slider').css('height', $('.trent-slider').width() / 2);
-		tSetCss();
-		//load bar 
-		tStartLoadBar();
-		$('.trent-slider').hover(function() {$('.t-load-bar .inner-load-bar').css($t_loadBarStopStyles);}, function() {tStartLoadBar()})
-		//interval sllide change
-		var tSlideChange = window.setInterval(function() {
+		$tSliderHeight = 650;
+	}
+	$(sliderName).css('height', $tSliderHeight);
+	setTextSlideHeight();
+	tSetCss();
+	//load bar 
+	tStartLoadBar();
+	$(sliderName).hover(function() {$(''+ sliderName +' .t-load-bar .inner-load-bar').css($t_loadBarStopStyles);}, function() {tStartLoadBar()})
+	//interval sllide change
+	var tSlideChange = window.setInterval(function() {
+		tSlideChangerRight();
+	}, 4500);
+	$(sliderName).mouseover(function() {
+		clearInterval(tSlideChange);
+	}).mouseout(function() {
+		tSlideChange = window.setInterval(function() {
 			tSlideChangerRight();
-		}, 4500);
-		$('.trent-slider').mouseover(function() {
-			clearInterval(tSlideChange);
-		}).mouseout(function() {
-			tSlideChange = window.setInterval(function() {
-				tSlideChangerRight();
-			},4500);
-		});
+		},4500);
+	});
 
-		// -----slider controls
-		//arrow
-		$('.t-slider-controls .arrow').click(function() {
-			if ($(this).hasClass('right-arrow')) {tSlideChangerRight();}
-			else if ($(this).hasClass('left-arrow')) {tSlideChangerLeft();}
-		});
-		//dots 
-		$('.t-slide-dots .t-dot').click(function() {
-			$newDotIndex = $(this).index();
-			$currentDotIndex = $('.current-dot').index();
-			if (tSliderHasStopped()) {
-				$('.t-slide').each(function(index, value) {
-					$('.current-dot').removeClass('current-dot');
-					$('.current-t-slide').removeClass('current-t-slide');
-					$('.t-dot').eq($newDotIndex).addClass('current-dot');
-					$('.t-slide').eq($newDotIndex).css($tSlideInStyles).addClass('current-t-slide');
-					if (index > $newDotIndex) {
-						$(this).css($hiddenSlideStylesRight);
-					} else if (index < $newDotIndex) {
-						$(this).css($hiddenSlideStylesLeft);
-					}
-				});
-			}
-		});
-		//close slider JS
+	// -----slider controls
+	//arrow
+	$(''+ sliderName +' .t-slider-controls .arrow').click(function() {
+		if ($(this).hasClass('right-arrow')) {tSlideChangerRight();}
+		else if ($(this).hasClass('left-arrow')) {tSlideChangerLeft();}
+	});
+	//dots 
+	$(''+ sliderName +' .t-slide-dots .t-dot').click(function() {
+		$newDotIndex = $(this).index();
+		$currentDotIndex = $(''+ sliderName +' .current-dot').index();
+		if (tSliderHasStopped()) {
+			$(''+ sliderName +' .t-slide').each(function(index, value) {
+				$(''+ sliderName +' .current-dot').removeClass('current-dot');
+				$(''+ sliderName +' .current-t-slide').removeClass('current-t-slide');
+				$(''+ sliderName +' .t-dot').eq($newDotIndex).addClass('current-dot');
+				$(''+ sliderName +' .t-slide').eq($newDotIndex).css($tSlideInStyles).addClass('current-t-slide');
+				if (index > $newDotIndex) {
+					$(this).css($hiddenSlideStylesRight);
+				} else if (index < $newDotIndex) {
+					$(this).css($hiddenSlideStylesLeft);
+				}
+			});
+			setTextSlideHeight();
+		}
+	});
+	//close slider JS
+}
+tRunSlider('.main-slider')
+tRunSlider('.secondary-slider');
+tRunSlider('.third-slider');
 });
